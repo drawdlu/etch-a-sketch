00071e85b8ef86e85537ba9e3eb856f710414f7a
drawdlu <152858099+drawdlu@users.noreply.github.com>
2024-09-05 18:16:21 +0800

    Create new grid based on user input

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const container = document.querySelector('.container');
+let previousGridSize = 16;
 
-function createRow() {
-    for (let i = 0; i < 16; i++) {
+function createGrid(size) {
+    for (let i = 0; i < size; i++) {
         const row = document.createElement('div');
         row.setAttribute('class', 'row');
         container.appendChild(row);
-        createBoxes(row);
+        createBoxes(row, size);
     }
 }
 
-function createBoxes(rowDiv) {
-    for (let i = 0; i < 16; i++) {
+function createBoxes(rowDiv, size) {
+    for (let i = 0; i < size; i++) {
         const box = document.createElement('div');
         box.setAttribute('class', 'box');
         rowDiv.appendChild(box)
@@ -34,13 +35,26 @@ function promptForNewSize() {
             newGridSize = prompt('Enter a grid size');
             firstPrompt = false;
         } else {
-            newGridSize = prompt('You can only enter a grid size with range of 0-100');
+            newGridSize = prompt('You can only enter a grid size with range of 1-100');
         }
         
-    } while (newGridSize < 0 || newGridSize > 100)
+    } while (newGridSize <= 0 || newGridSize > 100)
+
+    if (previousGridSize === newGridSize) {
+        return;
+    }
+    
+    removeOldGrid();
+    createGrid(newGridSize);
 }
 
+function removeOldGrid() {
+    const children = Array.from(container.children);
+    children.forEach( (child) => {
+        child.remove();
+    })
+}
 
-createRow();
+createGrid(16);
 
 listenToButton();
