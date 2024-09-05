93fcde2c51427f965452bf8169342fc90c67d5db
drawdlu <152858099+drawdlu@users.noreply.github.com>
2024-09-05 17:42:58 +0800

    Create grid with flex

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const container = document.querySelector('.container');
 
 function createRow() {
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < 16; i++) {
         const row = document.createElement('div');
         row.setAttribute('class', 'row');
         container.appendChild(row);
@@ -10,7 +10,7 @@ function createRow() {
 }
 
 function createBoxes(rowDiv) {
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < 16; i++) {
         const box = document.createElement('div');
         box.setAttribute('class', 'box');
         rowDiv.appendChild(box)
