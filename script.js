e509a11348609748046daa7b3c53c8de8ffc7bf1
drawdlu <152858099+drawdlu@users.noreply.github.com>
2024-09-05 17:30:07 +0800

    Create div elements for grid with no styles yet

---
+++ script.js
@@ -0,0 +1,20 @@
+const container = document.querySelector('.container');
+
+function createRow() {
+    for (let i = 0; i < 4; i++) {
+        const row = document.createElement('div');
+        row.setAttribute('class', 'row');
+        container.appendChild(row);
+        createBoxes(row);
+    }
+}
+
+function createBoxes(rowDiv) {
+    for (let i = 0; i < 4; i++) {
+        const box = document.createElement('div');
+        box.setAttribute('class', 'box');
+        rowDiv.appendChild(box)
+    }
+}
+
+createRow();
